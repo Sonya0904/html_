f9af3a1b69474362abf4d3ed675fe47c275b71e1
Sonya0904 <soph.zolotaryova01@gmail.com>
2025-11-06 18:51:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+console.log('hello world');
+
+let message;
+message='Hello!';
+alert(message);
+
+let favoriteFood="pizza";
+
+favoriteFood="syshi";
+
+const planet="Земля";
+
+let age = 20;
+age = 21;
+console.log(age);
+
+let $ = 1;
+let _ = 2;
+alert($ + _);
+
+let n = 123;
+n = 12.345;
+console.log(n);
+
+let isSunny = true; //Boolean is...
+let isRaining = false;
+
+if(isSunny){
+    console.log("go to walk");
+}
+
+let box=null;
+let anotherBox;
+console.log(box, anotherBox);
+
+let item = {
+    title: "book",
+    pages: 200,
+    available: true
+};
+console.log(item.title);
+
+let colors = ["red","green","blue"];
+console.log(colors[0]);
+
+let text = "Hello";
+console.log(typeof text);
+
+let color="blue";
+console.log(`my favorit color - ${color}!`);
