373215a5ac4caf2a7f30a9f5370f0094414f6d44
Sonya0904 <soph.zolotaryova01@gmail.com>
2025-11-11 18:48:37 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,147 @@
-console.log('hello world');
+// console.log('hello world');
 
-let message;
-message='Hello!';
-alert(message);
+// let message;
+// message='Hello!';
+// alert(message);
 
-let favoriteFood="pizza";
+// let favоriteFood="pizza";
 
-favoriteFood="syshi";
+// favoriteFood="syshi";
 
-const planet="Земля";
+// const planet="Земля";
 
-let age = 20;
-age = 21;
-console.log(age);
+// let age = 20;
+// age = 21;
+// console.log(age);
 
-let $ = 1;
-let _ = 2;
-alert($ + _);
+// let $ = 1;
+// let _ = 2;
+// alert($ + _);
 
-let n = 123;
-n = 12.345;
-console.log(n);
+// let n = 123;
+// n = 12.345;
+// console.log(n);
 
-let isSunny = true; //Boolean is...
-let isRaining = false;
+// let isSunny = true; //Boolean is...
+// let isRaining = false;
 
-if(isSunny){
-    console.log("go to walk");
+// if(isSunny){
+//     console.log("go to walk");
+// }
+
+// let bоx=null;
+// let anotherBox;
+// console.log(box, anotherBox);
+
+// let item = {
+//     title: "book",
+//     pages: 200,
+//     available: true
+// };
+// console.log(item.title);
+
+// let colоrs = ["red","green","blue"];
+// console.log(colors[0]);
+
+// let text = "Hello";
+// console.log(typeof text);
+
+// let color="blue";
+// console.log(`my favorit color - ${color}!`);
+
+// let buttonPressed = false;
+
+// if(buttonPressed){
+//     console.log("button");
+// }
+
+// let temp=0;
+// if(temp<0){
+//     console.log("-")
+// }
+// else{
+//     console.log("+")
+// }
+
+
+// let grade=8;
+
+//  if(grade>=9){
+//     console.log("отлично");
+//  } else if(grade>=7){
+//     console.log("хорошо");
+//  } else if(grade>=5){
+//     console.log("удовлетворительно");
+//  } else {
+//     console.log("нужно подтянуть");
+//  }
+
+
+//  let temprich=8;
+
+//  if(grade>=30){
+//     console.log("very hot");
+//  } else if(grade>=20){
+//     console.log("hot");
+//  } else if(grade>=10){
+//     console.log("warm");
+//  } else {
+//     console.log("cold");
+//  }
+
+
+// let ageage=17;
+// let hasTicket = true;
+
+// if(ageage>=18 && hasTicket){
+//    console.log("open");
+// } else if(ageage>=18 && !hasTicket){
+//    consolе.log("close");
+// } else{
+//     console.log("rasti");
+// }
+
+
+// let day = 3;
+// switch (day) {
+//     case 1:
+//     consоle.log ("Понедельник");
+//     break;
+// case 2:
+//     console.log ("Вторник");
+//     break;
+// case 3:
+//     console.log ("Среда");
+//     break;
+// default:
+//     console.log ("Другой день");
+//     }
+
+let role="admin"
+
+switch (role) {
+    case "admin":
+    console. log ("hello, admin");
+    break;
+case "user":
+    console.log ("hello,user");
+    break;
+case "guest":
+    console.log ("hello,guest");
+    break;
+default:
+    console. log ("other people");
 }
 
-let box=null;
-let anotherBox;
-console.log(box, anotherBox);
+let isOnline = true;
+let status = isOnline? "online":"ofline";
+console.log(status);
 
-let item = {
-    title: "book",
-    pages: 200,
-    available: true
-};
-console.log(item.title);
+let isReady = true;
+let statZakaz = isReady ? "ready" : "no";
+console.log(statZakaz);
 
-let colors = ["red","green","blue"];
-console.log(colors[0]);
 
-let text = "Hello";
-console.log(typeof text);
 
-let color="blue";
-console.log(`my favorit color - ${color}!`);
+
+
